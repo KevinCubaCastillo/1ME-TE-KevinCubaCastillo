2fef125062f467ed260f5aef672f2f434e7c129e
Kevin Cuba Castillo <cck2023657@est.univalle.edu>
2023-09-26 17:27:42 -0400

    actualizacion arqui

---
+++ 1ME-TE-KevinCubaCastillo/Controllers/ComputerController.cs
@@ -0,0 +1,27 @@
+﻿using LN;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _1ME_TE_KevinCubaCastillo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComputerController : ControllerBase
+    {
+        private IComputerService _computerService;
+        public ComputerController(IComputerService computerService)
+        {
+            _computerService = computerService;
+        }
+        [HttpGet ("getById")]           
+        public IActionResult getById(int id)
+        {
+            return Ok();
+        }
+        [HttpPost ("addComputer")]
+        public IActionResult addComputer()
+        {
+            return Ok();
+        }
+    }
+}
